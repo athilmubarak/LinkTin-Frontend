02f390222566635e78f19cffa98b87c5e35b782c
athilmubarak <amubarak@integretz.com>
2024-06-07 12:32:23 +0530

    Ansar build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export const environment = {
     production: true,
-    api_url: 'http://192.168.251.168/api',
+    api_url: 'http://192.168.18.172:81/api',
     url: `${window.location.origin}`,
 };
